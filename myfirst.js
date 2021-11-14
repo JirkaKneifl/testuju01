cb2fe3be697be34f4e0875b257e18127a0fbc575
JirkaKneifl <Jirka.kneifl@email.cz>
2021-11-14 19:17:23 +0100

    pridal jsem myfirst.js , myfirstmodule.js

---
+++ myfirst.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var dt = require('./myfirstmodule');
+
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(dt.myDatetime());
+  res.end();
+  
+  
+}).listen(8080); 
+

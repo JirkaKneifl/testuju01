f4ce658e7e9331d43ead4869464095e9f2cf7a88
JirkaKneifl <Jirka.kneifl@email.cz>
2021-12-15 11:56:57 +0100

    myfirstmodule.js -> propojil jsem myfirst.js a myfirstmodule.js

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var dt = require('./myfirstmodule');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("Dnešní datum je: " + dt.myDateTime());
   res.end('Hello World!');
 }).listen(8080);

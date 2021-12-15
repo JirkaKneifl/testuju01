c52b122d05665fcb30266d7fe852413059df2876
JirkaKneifl <Jirka.kneifl@email.cz>
2021-12-15 19:46:01 +0100

    myfirst.js -> udelal jsem mezeru XD

--- myfirst.js
+++ myfirst.js
@@ -3,6 +3,7 @@ var dt = require('./myfirstmodule');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
+  
   res.write("Dnesni datum je: " + dt.myDateTime());
   res.end('Hello World!');
 }).listen(8080);

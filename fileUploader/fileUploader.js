a1054ef0e0cf4fa8a3b2804e5efb9900fc448cb6
JirkaKneifl <Jirka.kneifl@email.cz>
2021-12-16 18:44:43 +0100

    fileUploader.js -> uprava - prejmenovani stare cesty do nove na kterou se má potom vybrany sobor ulozit

--- fileUploader/fileUploader.js
+++ fileUploader/fileUploader.js
@@ -1,10 +1,16 @@
 var http = require('http');
 var formidable = require('formidable');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
     if (req.url == '/fileupload') {
         var form = new formidable.IncomingForm();
         form.parse(req, function (err, fields, files) {
+          var staraCesta = files.filetoupload.filepath;
+          var novaCesta = 'C:/xampp/htdocs/testuju01/fileUploader/'+ files.filetoupload.originalFilename;;
+          fs.rename(staraCesta, novaCesta, function(err){
+              if(err) throw err;
+          });
           res.write('File uploaded');
           res.end();
         });

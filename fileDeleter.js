6213680b3cc8e9721cc5f96c6c833ed75e074ec0
JirkaKneifl <Jirka.kneifl@email.cz>
2021-12-16 14:40:03 +0100

    fileDeleter.js -> uprava deleter vymaze jen soubor mynewfile2.txt -> predchozi kommit byl spatny

--- fileDeleter.js
+++ fileDeleter.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 
-fs.unlink('mynewfile1.txt', 'mynewfile2.txt', 'mynewfile3.txt', function (err) {
+fs.unlink('mynewfile2.txt', function (err) {
   if (err) throw err;
   console.log('File deleted!');
 });

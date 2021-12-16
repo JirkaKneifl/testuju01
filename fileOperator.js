5bd6398fa4020924a09e4afdf8d2230edf71baf2
JirkaKneifl <Jirka.kneifl@email.cz>
2021-12-16 14:25:39 +0100

    fileOperator.js -> pridani funkci => readFile() , appendFile() , open() , writeFile()

--- fileOperator.js
+++ fileOperator.js
@@ -2,10 +2,25 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(req, res) {
-    fs.readFile('demoFile1.html', function(err , data){
+      fs.readFile('demoFile1.html', function(err , data){ 
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         return res.end();
-    });
+      });
+
+      fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
+
+      fs.open('mynewfile2.txt', 'w', function (err, file) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
+
+      fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
 }).listen(8080);
 
